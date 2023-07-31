a61ff797a819c6d5642da7b334ad2fa6f6c93ca4
SmArtKar <smart.karpov@gmail.com>
2023-07-31 21:57:29 +0300

    two custom comps

---
+++ Source/StonebornDelights/HediffComp_Hangover.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace StonebornDelights
+{
+    public class HediffComp_Hangover : HediffComp
+    {
+        private HediffCompProperties_Hangover Props => props as HediffCompProperties_Hangover;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (parent.CurStageIndex < Props.minHangoverStage || !Pawn.IsHashIntervalTick(300))
+            {
+                return;
+            }
+
+            Hediff hediff = Pawn.health.hediffSet.GetFirstHediffOfDef(Props.hangover);
+
+            if (hediff != null)
+            {
+                hediff.Severity = 1f;
+                return;
+            }
+
+            hediff = HediffMaker.MakeHediff(Props.hangover, Pawn);
+            hediff.Severity = 1f;
+            Pawn.health.AddHediff(hediff);
+        }
+    }
+
+    public class HediffCompProperties_Hangover : HediffCompProperties
+    {
+        public HediffDef hangover;
+        public int minHangoverStage = 3;
+
+        public HediffCompProperties_Hangover()
+        {
+            compClass = typeof(HediffComp_Hangover);
+        }
+    }
+}
---
+++ Source/StonebornDelights/HediffComp_HiddenWith.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace StonebornDelights
+{
+    public class HediffComp_HiddenWith : HediffComp
+    {
+        private HediffCompProperties_HiddenWith Props => props as HediffCompProperties_HiddenWith;
+
+        public override bool CompDisallowVisible()
+        {
+            if (base.CompDisallowVisible())
+            {
+                return true;
+            }
+
+            for (int i = Props.hiderHediffs.Count - 1; i >= 0; i--)
+            {
+                if (Pawn.health.hediffSet.HasHediff(Props.hiderHediffs[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class HediffCompProperties_HiddenWith : HediffCompProperties
+    {
+        public List<HediffDef> hiderHediffs;
+
+        public HediffCompProperties_HiddenWith()
+        {
+            compClass = typeof(HediffComp_HiddenWith);
+        }
+    }
+}
