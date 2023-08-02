fe609fbeb417cfcca97ebde666196cda26523bc7
Elseud <83170940+Elseud@users.noreply.github.com>
2023-08-02 20:26:33 +0200

    Added outcome doer for removing hangovers

---
+++ Source/StonebornDelights/IngestionOutcomeDoer_RemoveHangover.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace StonebornDelights
+{
+	public class IngestionOutcomeDoer_RemoveHangover : IngestionOutcomeDoer
+	{
+		protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+		{
+			Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Hangover);
+			if (firstHediffOfDef != null)
+			{
+				pawn.health.RemoveHediff(firstHediffOfDef);
+			}
+		}
+	}
+}
