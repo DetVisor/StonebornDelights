5f2ae37bf3b1fda3afa1ca78554ecd529c2f8c1c
SmArtKar <smart.karpov@gmail.com>
2023-08-02 21:42:45 +0300

    Fixes hangover removal

--- Source/StonebornDelights/IngestionOutcomeDoer_RemoveHangover.cs
+++ Source/StonebornDelights/IngestionOutcomeDoer_RemoveHangover.cs
@@ -5,9 +5,9 @@ namespace StonebornDelights
 {
 	public class IngestionOutcomeDoer_RemoveHangover : IngestionOutcomeDoer
 	{
-		protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+		public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
 		{
-			Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Hangover);
+			Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(SD_DefOf.SD_LightHangover);
 			if (firstHediffOfDef != null)
 			{
 				pawn.health.RemoveHediff(firstHediffOfDef);
---
+++ Source/StonebornDelights/SD_DefOf.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+[DefOf]
+public class SD_DefOf
+{
+    static SD_DefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(SD_DefOf));
+    }
+
+    public static HediffDef SD_LightHangover;
+}
