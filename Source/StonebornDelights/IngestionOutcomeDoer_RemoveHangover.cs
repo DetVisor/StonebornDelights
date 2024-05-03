7dcddcb55dfedcb60910b2bab96de894bc2cb5d2
SmArtKar <smart.karpov@gmail.com>
2024-05-03 15:41:56 +0300

    Ref update, 1.5 recompile

--- Source/StonebornDelights/IngestionOutcomeDoer_RemoveHangover.cs
+++ Source/StonebornDelights/IngestionOutcomeDoer_RemoveHangover.cs
@@ -5,13 +5,13 @@ namespace StonebornDelights
 {
 	public class IngestionOutcomeDoer_RemoveHangover : IngestionOutcomeDoer
 	{
-		public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
-		{
+		public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
 			Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(SD_DefOf.SD_LightHangover);
 			if (firstHediffOfDef != null)
 			{
 				pawn.health.RemoveHediff(firstHediffOfDef);
 			}
 		}
-	}
+    }
 }
